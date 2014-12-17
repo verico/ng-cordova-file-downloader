dd6094d7240eb6e34b71ece7365dc6d467afe144
andwaal <a_waal@hotmail.com>
2014-12-17 19:20:32 +0100

    Removed leftover variable

--- src/downloadOperations/file-downloader-single.js
+++ src/downloadOperations/file-downloader-single.js
@@ -30,17 +30,11 @@
                         uri,
                         path,
                         function(entry) {
-                            if(currentTimeout){
-                                $timeout.cancel(currentTimeout);
-                            }
-
                             if (typeof entry.toURL == 'function') {
                                 deferred.resolve(entry.toURL());
                             }else{
                                 deferred.resolve(entry.fullPath);
                             }
-
-
                         },
                         function(error) {
                             deferred.reject('Image download failed:' + JSON.stringify(error));
